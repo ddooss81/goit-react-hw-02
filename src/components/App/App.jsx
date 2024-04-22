ae15b16d6cb88934752e2b36c68b524a6b62403b
ddooss81 <144255208+ddooss81@users.noreply.github.com>
2024-04-22 14:02:32 +0300

    111

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react';
-import {useEffect} from 'react';
-import './App.css'
+import { useEffect } from 'react';
+import './App.css';
 import Description from '../Description/Description.jsx';
 import Options from '../Options/Options';
 import Feedback from '../Feedback/Feedback';
@@ -8,28 +8,28 @@ import Notification from '../Notification/Notification';
 import clsx from 'clsx';
 
 export default function App() {
- const initialFeedback = JSON.parse(localStorage.getItem('feedback')) || {
+  const initialFeedback = JSON.parse(localStorage.getItem('feedback')) || {
     good: 0,
     neutral: 0,
     bad: 0
   };
 
-  const [values, setValues] = useState(initialFeedback);
+  const [values, setValues] = useState(() => initialFeedback);
 
   useEffect(() => {
     localStorage.setItem('feedback', JSON.stringify(values));
   }, [values]);
 
-const totalFeedback = values.good + values.neutral + values.bad;
-const positiveFeedback = Math.round(((values.good +values.neutral)/ totalFeedback) * 100)+'%';
+  const totalFeedback = values.good + values.neutral + values.bad;
+  const positiveFeedback = Math.round((values.good / totalFeedback) * 100) + '%';
 
-const updateFeedback = feedbackType => {
+  const updateFeedback = (feedbackType) => {
     setValues({
       ...values,
       [feedbackType]: values[feedbackType] + 1
     });
-};
-  
+  };
+
   const resetFeedback = () => {
     setValues({
       good: 0,
@@ -38,15 +38,15 @@ const updateFeedback = feedbackType => {
     });
   };
 
-  
   return (
     <>
       <Description />
-      <Options updateFeedback={updateFeedback} totalFeedback={totalFeedback} resetFeedback={resetFeedback}/>
-       {totalFeedback > 0 ? <Feedback values={values} totalFeedback={totalFeedback} positiveFeedback={positiveFeedback}/> : <Notification />}       
-
+      <Options updateFeedback={updateFeedback} totalFeedback={totalFeedback} resetFeedback={resetFeedback} />
+      {totalFeedback > 0 ? (
+        <Feedback values={values} totalFeedback={totalFeedback} positiveFeedback={positiveFeedback} />
+      ) : (
+        <Notification />
+      )}
     </>
   );
-};
-
-
+}
